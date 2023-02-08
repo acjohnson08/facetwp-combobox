f8f2114cea774d87a5cdf7c0e6d3ee3658cea62f
Andrew Johnson <78057108+acjohnson08@users.noreply.github.com>
2023-02-08 13:09:30 -0600

    Update front.js
    Enabled the fullTextSearch option for the Semantic UI dropdown and updated indentation for readability.

--- assets/js/front.js
+++ assets/js/front.js
@@ -1,23 +1,27 @@
 (function($) {
-    // Add a refresh action for the combobox facet
-    FWP.hooks.addAction('facetwp/refresh/combobox', function($this, facet_name) {
-        var val = $this.find('.facetwp-combobox').val();
-        FWP.facets[facet_name] = val ? [val] : [];
-    });
-    
+	// Add a refresh action for the combobox facet
+	FWP.hooks.addAction('facetwp/refresh/combobox', function($this, facet_name) {
+		var val = $this.find('.facetwp-combobox').val();
+		FWP.facets[facet_name] = val ? val.split(',') : [];
+	});
+	
 	$(document).on('change', '.facetwp-type-combobox input[type="hidden"]', function() {
-        var $facet = $(this).closest('.facetwp-facet');
-        var facet_name = $facet.attr('data-name');
-
-        if ('' !== $(this).val()) {
-            FWP.frozen_facets[facet_name] = 'soft';
-        }
-        FWP.autoload();
-    });
+		var $facet = $(this).closest('.facetwp-facet');
+		var facet_name = $facet.attr('data-name');
+		
+		if ('' !== $(this).val()) {
+			FWP.frozen_facets[facet_name] = 'soft';
+		}
+		FWP.autoload();
+	});
 })(fUtil);
 
 (function($) {
-    $(document).on('facetwp-loaded', function() {
-		$('.ui.dropdown').dropdown({forceSelection: false, clearable: true});
-    });
+	$(document).on('facetwp-loaded', function() {
+		$('.ui.dropdown').dropdown( {
+			forceSelection: false,
+			clearable: true,
+			fullTextSearch: true
+		} );
+	});
 })(jQuery);
